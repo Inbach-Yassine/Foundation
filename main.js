6ecfd43c3809843d5f155938c9c43167a5373b21
YaSsyn_Inba <111376798+Inbach-Yassine@users.noreply.github.com>
2023-05-07 21:48:18 +0100

    updates

--- main.js
+++ main.js
@@ -4,5 +4,4 @@ var Toggele = document.getElementById("toggele");
 Toggele.addEventListener("click", () => {
   Menu.classList.toggle("open");
   Toggele.classList.toggle("open");
-  document.body.style.overflowY= "hidden";
 });
