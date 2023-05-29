282710641e1f89b22b253be54536076db33af9cf
YaSsyn_Inba <111376798+Inbach-Yassine@users.noreply.github.com>
2023-05-29 21:42:32 +0100

    index.css

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 var Menu = document.querySelector(".menu-bar");
 var Toggele = document.getElementById("toggele");
+var BtnScroll = document.querySelector(".btn-scroll-top");
+var Scroll = document.getElementById("scroll-top");
 
 Toggele.addEventListener("click", () => {
   Menu.classList.toggle("open");
   Toggele.classList.toggle("open");
 });
+
+window.addEventListener("scroll" , () => {
+  if (window.pageYOffset > 600 ){
+    BtnScroll.classList.add("active");
+  }
+  else{
+    BtnScroll.classList.remove("active");
+  }
+})
